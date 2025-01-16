e966d6b449f80aa808e17fc7e622fa488a9470d2
Stefan Kummerlöw <s.kummerloew@gmail.com>
2025-01-16 16:43:12 +0100

    style(layout.tsx): update body className to include additional background styles for improved design aesthetics
    style(promptgenerator.tsx): improve code readability by formatting array elements and object properties in a more organized manner
    feat(promptgenerator.tsx): update Card and CardTitle styles for better visual presentation
    feat(promptgenerator.tsx): update CardDescription styles for improved readability
    feat(promptgenerator.tsx): update Select and Checkbox label styles for better alignment
    feat(promptgenerator.tsx): update SelectItem and Checkbox label styles for better readability
    feat(promptgenerator.tsx): update CardTitle font size and color for enhanced visual appeal
    feat(promptgenerator.tsx): update CardDescription text size and color for improved readability
    feat(promptgenerator.tsx): update Card and form width for better layout consistency
    feat(promptgenerator.tsx): update Link text for better clarity and user experience
    feat(promptgenerator.tsx): update form width for improved layout consistency and responsiveness
    
    style(promptgenerator.tsx): reformat code for better readability and maintainability by adding line breaks and consistent indentation.
    
    feat: add new photography categories and 3D techniques to the selection options
    feat: include additional display types in the available options for selection
    
    style(promptgenerator.tsx): improve code formatting and readability by properly indenting and aligning code blocks
    
    style(promptgenerator.tsx): improve code formatting by aligning object properties and values for better readability
    
    style(promptgenerator.tsx): improve code formatting for better readability and consistency
    
    style(promptgenerator.tsx): improve styling of the final prompt section by changing background color and border styles for better visual appeal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased absolute inset-0 h-full w-full bg-white bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]`}
       >
         {children}
       </body>
